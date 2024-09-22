8fafb9419d7a5f3d4c24be630969807b5cb9659d
Navnit rai <99525523+Navnit73@users.noreply.github.com>
2024-09-22 20:42:29 +0530

    Update Subscribe.jsx

--- src/components/Payments/Subscribe.jsx
+++ src/components/Payments/Subscribe.jsx
@@ -13,7 +13,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { buySubscription } from '../../redux/actions/user';
 import { server } from '../../redux/store';
 import toast from 'react-hot-toast';
-import logo from '../../assets/images/logo.png';
+//import logo from '../../assets/images/logo.png';
 
 const Subscribe = ({ user }) => {
   const dispatch = useDispatch();
@@ -48,7 +48,7 @@ const Subscribe = ({ user }) => {
           key,
           name: 'Learnx',
           description: 'Get access to all premium content',
-          image: logo,
+         
           subscription_id: subscriptionId,
           callback_url: `${server}/paymentverification`,
           prefill: {
